749cd3b33dccf4505134f9f0daa1b89b1c8165e3
Oleg Lobachev <aironik@gmail.com>
2015-06-11 14:43:51 +0300

    AirSettingsViewController initial commit

---
+++ AirSettingsViewController/AirSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AirSettingsViewController.h
+//  AirSettingsViewController
+//
+//  Created by Oleg Lobachev on 11.06.15.
+//  Copyright (c) 2015 aironik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AirSettingsViewController : NSObject
+
+@end
---
+++ AirSettingsViewController/AirSettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  AirSettingsViewController.m
+//  AirSettingsViewController
+//
+//  Created by Oleg Lobachev on 11.06.15.
+//  Copyright (c) 2015 aironik. All rights reserved.
+//
+
+#import "AirSettingsViewController.h"
+
+@implementation AirSettingsViewController
+
+@end
