31c25336892d648daefb067b640b5f818284bd77
Oleg Lobachev <aironik@gmail.com>
2015-06-14 13:47:14 +0300

    Settings Action

---
+++ AirTableViewController/SettingsViewController/Values/Action/AITSettingsActionCell.m
@@ -0,0 +1,33 @@
+//
+//  AITSettingsActionCell.m
+//  AirTableViewController
+//
+//  Created by Oleg Lobachev aironik@gmail.com on 13.06.15.
+//  Copyright © 2015 aironik. All rights reserved.
+//
+
+#import "AITSettingsActionCell.h"
+
+#import "AITCellEtchedView.h"
+#import "AITTableViewCellOverride.h"
+
+
+#if !(__has_feature(objc_arc))
+#error ARC required. Add -fobjc-arc compiler flag for this file.
+#endif
+
+
+@interface AITSettingsActionCell ()
+@end
+
+
+#pragma mark - Implementation
+
+
+@implementation AITSettingsActionCell
+
+
+AIT_CELL_OVERRIDE_ACTION
+
+
+@end
---
+++ AirTableViewController/SettingsViewController/Values/Action/AITSettingsActionValue.h
@@ -0,0 +1,17 @@
+//
+//  AITSettingsActionValue.h
+//  AirTableViewController
+//
+//  Created by Oleg Lobachev aironik@gmail.com on 13.06.15.
+//  Copyright © 2015 aironik. All rights reserved.
+//
+
+#import <AirTableViewController/Values/ValuesWithState/Action/AITActionValue.h>
+
+
+/**
+ * @brief The cell that override AITActionValue behaviour
+ * @details Define narrow cell for iOS 7 and higher and usual width otherwise
+ */
+@interface AITSettingsActionValue : AITActionValue
+@end
