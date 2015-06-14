79b092405b31102db2c46ec9ff97b633f62752c7
Oleg Lobachev <aironik@gmail.com>
2015-06-14 17:02:04 +0300

    Settings Header and Footer

---
+++ AirTableViewController/SettingsViewController/HeaderFooter/AITSettingsFooterView.h
@@ -0,0 +1,14 @@
+//
+//  AITSettingsFooterView.h
+//  AirTableViewController
+//
+//  Created by Oleg Lobachev aironik@gmail.com on 13.06.15.
+//  Copyright © 2015 aironik. All rights reserved.
+//
+
+#import <AirTableViewController/HeaderFooter/AITHeaderFooterView.h>
+
+
+@interface AITSettingsFooterView : AITHeaderFooterView
+
+@end
---
+++ AirTableViewController/SettingsViewController/HeaderFooter/AITSettingsFooterView.m
@@ -0,0 +1,22 @@
+//
+//  AITSettingsFooterView.m
+//  AirTableViewController
+//
+//  Created by Oleg Lobachev aironik@gmail.com on 13.06.15.
+//  Copyright © 2015 aironik. All rights reserved.
+//
+
+#import "AITSettingsFooterView.h"
+
+@implementation AITSettingsFooterView
+
+- (void)updateTintColorSelector:(UIColor *)newColor {
+    if (self.actionBlock != NULL) {
+        self.label.textColor = AIT_COLOR_TINT;
+    }
+    else {
+        self.label.textColor = AIT_COLOR_SETTINGS_FOOTER_TEXT;
+    }
+}
+
+@end
