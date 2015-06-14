46dcfbc8ce85922cfc127f06b2127814961e6b3f
Oleg Lobachev <aironik@gmail.com>
2015-06-14 14:30:55 +0300

    Settings Pending Operation

---
+++ AirTableViewController/SettingsViewController/Values/PendingOperation/AITSettingsPendingOperationCell.m
@@ -0,0 +1,38 @@
+//
+//  AITSettingsPendingOperationCell.m
+//  AirTableViewController
+//
+//  Created by Oleg Lobachev aironik@gmail.com on 13.06.15.
+//  Copyright © 2015 aironik. All rights reserved.
+//
+
+#import "AITSettingsPendingOperationCell.h"
+
+#import "AITCellEtchedView.h"
+#import "AITTableViewCellOverride.h"
+
+
+#if !(__has_feature(objc_arc))
+#error ARC required. Add -fobjc-arc compiler flag for this file.
+#endif
+
+
+@interface AITSettingsPendingOperationCell ()
+@end
+
+
+#pragma mark - Implementation
+
+
+@implementation AITSettingsPendingOperationCell
+
+
+AIT_CELL_OVERRIDE_STATIC
+
+
++ (CGFloat)preferredHeightForValue:(AITValue *)value {
+    return 88.;
+}
+
+
+@end
---
+++ AirTableViewController/SettingsViewController/Values/PendingOperation/AITSettingsPendingOperationValue.m
@@ -0,0 +1,27 @@
+//
+//  AITSettingsPendingOperationValue.m
+//  AirTableViewController
+//
+//  Created by Oleg Lobachev aironik@gmail.com on 13.06.15.
+//  Copyright © 2015 aironik. All rights reserved.
+//
+
+#import "AITSettingsPendingOperationValue.h"
+
+
+#if !(__has_feature(objc_arc))
+#error ARC required. Add -fobjc-arc compiler flag for this file.
+#endif
+
+
+@interface AITSettingsPendingOperationValue ()
+@end
+
+
+#pragma mark - Implementation
+
+
+@implementation AITSettingsPendingOperationValue
+
+
+@end
