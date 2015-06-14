5536374db421dc9eee0ee9b2d82c4ac484c8c94c
Oleg Lobachev <aironik@gmail.com>
2015-06-14 14:28:00 +0300

    Settings Details

---
+++ AirTableViewController/SettingsViewController/Values/Details/AITSettingsDetailsCell.m
@@ -0,0 +1,33 @@
+//
+//  AITSettingsDetailsCell.m
+//  AirTableViewController
+//
+//  Created by Oleg Lobachev aironik@gmail.com on 13.06.15.
+//  Copyright © 2015 aironik. All rights reserved.
+//
+
+#import "AITSettingsDetailsCell.h"
+
+#import "AITCellEtchedView.h"
+#import "AITTableViewCellOverride.h"
+
+
+#if !(__has_feature(objc_arc))
+#error ARC required. Add -fobjc-arc compiler flag for this file.
+#endif
+
+
+@interface AITSettingsDetailsCell ()
+@end
+
+
+#pragma mark - Implementation
+
+
+@implementation AITSettingsDetailsCell
+
+
+AIT_CELL_OVERRIDE_DETAILED
+
+
+@end
---
+++ AirTableViewController/SettingsViewController/Values/Details/AITSettingsDetailsValue.h
@@ -0,0 +1,17 @@
+//
+//  AITSettingsDetailsValue.h
+//  AirTableViewController
+//
+//  Created by Oleg Lobachev aironik@gmail.com on 13.06.15.
+//  Copyright © 2015 aironik. All rights reserved.
+//
+
+#import <AirTableViewController/Values/ValuesWithState/Details/AITDetailsValue.h>
+
+
+/**
+ * @brief The cell that override AITDetailsValue behaviour
+ * @details Define narrow cell for iOS 7 and higher and usual width otherwise
+ */
+@interface AITSettingsDetailsValue : AITDetailsValue
+@end
